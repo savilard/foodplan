12c19957397cb40fda391eacd00c8608e64802a1
savilard <vitaliyantonoff@gmail.com>
2022-08-02 10:29:22 +0600

    :sparkles: Setup DefaultModel and TimestampedModel

--- backend/apps/core/models.py
+++ backend/apps/core/models.py
@@ -1,3 +1,25 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+from behaviors.behaviors import Timestamped
+
+
+class DefaultModel(models.Model):
+    """Project default model."""
+
+    uuid = models.UUIDField(db_index=True, unique=True, default=uuid.uuid4)
+
+    class Meta:
+        abstract = True
+
+
+class TimestampedModel(DefaultModel, Timestamped):
+    """
+    Default app model that has `created` and `updated` attributes.
+
+    Currently based on https://github.com/audiolion/django-behaviors
+    """
+
+    class Meta:
+        abstract = True
