395c4a6143ac99486355b9df27cee47188a6aa9e
savilard <vitaliyantonoff@gmail.com>
2022-08-09 17:30:06 +0600

    :sparkles: Setup custom user model

--- backend/apps/users/admin.py
+++ backend/apps/users/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserChangeForm
+from .forms import CustomUserCreationForm
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = (
+        'username',
+        'email',
+        'is_staff',
+        'is_active',
+    )
+    list_filter = (
+        'email',
+        'is_staff',
+        'is_active',
+    )
+    fieldsets = (
+        (
+            None,
+            {
+                'fields': (
+                    'first_name',
+                    'last_name',
+                    'email',
+                    'username',
+                    'password',
+                ),
+            },
+        ),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+        (
+            'Техническая информация',
+            {
+                'classes': ('collapse',),
+                'fields': ('uuid',),
+            },
+        ),
+    )
+    add_fieldsets = (
+        (None, {'classes': ('wide',), 'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}),
+    )
+    search_fields = ('email', 'username')
+    ordering = ('email',)
+    readonly_fields = ('uuid',)
--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from apps.core.models import TimestampedModel
+
+
+class CustomUser(AbstractUser, TimestampedModel):
+    """Custom user model."""
+
+    pass
+
+    def __str__(self):
+        return self.username
