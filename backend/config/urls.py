d550ea9a58a6387ae86163e70a0a416bb9e805e7
savilard <vitaliyantonoff@gmail.com>
2022-08-10 07:42:43 +0600

    :recycle: Add djoser urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -5,4 +5,6 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('api/auth/', include('djoser.urls')),
+    path('api/auth/', include('djoser.urls.authtoken')),
 ]
