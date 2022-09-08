3313d48d85f31698b12343a349171e84ae6c010c
savilard <vitaliyantonoff@gmail.com>
2022-09-08 18:21:08 +0600

    :recycle: Remove cast=bool from vars

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -1,4 +1,4 @@
 from config.settings.environs import env
 
 SECRET_KEY = env('SECRET_KEY')
-DEBUG = env('DEBUG', cast=bool, default=False)
+DEBUG = env('DEBUG', default=False)
--- backend/config/settings/static.py
+++ backend/config/settings/static.py
@@ -1,4 +1,4 @@
 from config.settings.environs import env
 
 STATIC_URL = '/static/'
-STATIC_ROOT = env('STATIC_ROOT', cast=str, default='static')
+STATIC_ROOT = env('STATIC_ROOT', default='static')
