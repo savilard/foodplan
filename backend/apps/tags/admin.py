99509e0c530e49e30cf0e2cf1026d8bf3bfb2a06
savilard <vitaliyantonoff@gmail.com>
2022-09-18 15:45:56 +0600

    :sparkles: Setup tag admin panel

--- backend/apps/tags/admin.py
+++ backend/apps/tags/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.tags.models import Tag
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    fieldsets = (
+        (
+            None,
+            {
+                'fields': (
+                    'name',
+                    'color',
+                    'slug',
+                ),
+            },
+        ),
+        (
+            'Техническая информация',
+            {
+                'classes': (
+                    'collapse',
+                ),
+                'fields': (
+                    'uuid',
+                ),
+            },
+        ),
+    )
+
+    readonly_fields = (
+        'uuid',
+    )
+    list_display = (
+        'name',
+        'slug',
+    )
+    search_fields = (
+        'name',
+    )
