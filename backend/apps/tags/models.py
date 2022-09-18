9e40a631a98201bc171a913e6a261e5aafba7f2d
savilard <vitaliyantonoff@gmail.com>
2022-09-18 16:34:57 +0600

    :rotating_light: Make flake8 happy

--- backend/apps/tags/models.py
+++ backend/apps/tags/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-from django.db.models import functions
 from django.utils.html import format_html
 from django.utils.translation import gettext_lazy as _
 
 from apps.core.models import TimestampedModelWithUUID
 
-models.CharField.register_lookup(functions.Length, 'len')
+models.CharField.register_lookup(models.functions.Length, 'len')
 
 
 class Tag(TimestampedModelWithUUID):
