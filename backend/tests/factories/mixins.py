00adc3f391e0a5bf477b06878f440fc86317f062
savilard <vitaliyantonoff@gmail.com>
2022-09-19 13:46:01 +0600

    :rotating_light: Make flake8 happy

--- backend/tests/factories/mixins.py
+++ backend/tests/factories/mixins.py
@@ -5,6 +5,8 @@ from factory.builder import BuildStep
 
 
 class UniqueStringMixin(Faker):
+    """Mixin for creating guaranteed unique values for text fields with unique=True."""
+
     def evaluate(self, instance: Any, step: BuildStep, extra: Any) -> str:
-        value = super().evaluate(instance, step, extra)
-        return f'{step.sequence} {value}'
+        text_char_value = super().evaluate(instance, step, extra)
+        return f'{step.sequence} {text_char_value}'
--- backend/tests/test_apps/test_tags/test_models.py
+++ backend/tests/test_apps/test_tags/test_models.py
@@ -8,7 +8,11 @@ pytestmark = [pytest.mark.django_db]
 
 
 def test_name_color_unique_together(tag_factory: TagFactory) -> None:
-    """Checking the uniqueness of the name and color fields together."""
+    """Checking the uniqueness of the name and color fields together.
+
+    Args:
+        tag_factory: pytest tag factory
+    """
     with pytest.raises(IntegrityError):
         tag_factory.create_batch(
             name='Завтрак',
@@ -21,13 +25,20 @@ def test_name_color_unique_together(tag_factory: TagFactory) -> None:
 @pytest.mark.parametrize('tag_name, tag_color, tag_slug', [
     ('', '#55A15E', 'breakfast'),
     ('Завтрак', '#55A15E', ''),
-    ('Завтрак', '55A15E', 'breakfast'),
-    ('Завтрак', '#', 'breakfast'),
-    ('Завтрак', '#5', 'breakfast'),
-    ('Завтрак', '#55', 'breakfast'),
+    ('Обед', '55A15E', 'lunch'),
+    ('Ужин', '#', 'dinner'),
+    ('Перекус', '#5', 'snack-time'),
+    ('Полдник', '#55', 'afternoon-snack'),
 ])
 def test_model_check_constraint(tag_name: str, tag_color: str, tag_slug: str, tag_factory: TagFactory) -> None:
-    """Test tag model CheckConstraint."""
+    """Test tag model CheckConstraint.
+
+    Args:
+        tag_name: tag name
+        tag_color: tag color
+        tag_slug: tag slug
+        tag_factory: pytest tag factory
+    """
     with pytest.raises(IntegrityError):
         tag_factory.create(
             name=tag_name,
