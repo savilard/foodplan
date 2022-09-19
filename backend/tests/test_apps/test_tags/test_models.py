716afaa764a5c76f15919148d2c0e595a87501ef
savilard <vitaliyantonoff@gmail.com>
2022-09-19 13:34:46 +0600

    :white_check_mark: Add tag model tests

---
+++ backend/tests/test_apps/test_tags/test_models.py
@@ -0,0 +1,36 @@
+import pytest
+
+from django.db import IntegrityError
+
+from tests.factories.tag_factory import TagFactory
+
+pytestmark = [pytest.mark.django_db]
+
+
+def test_name_color_unique_together(tag_factory: TagFactory) -> None:
+    """Checking the uniqueness of the name and color fields together."""
+    with pytest.raises(IntegrityError):
+        tag_factory.create_batch(
+            name='Завтрак',
+            color='#55A15E',
+            slug='breakfast',
+            size=2,
+        )
+
+
+@pytest.mark.parametrize('tag_name, tag_color, tag_slug', [
+    ('', '#55A15E', 'breakfast'),
+    ('Завтрак', '#55A15E', ''),
+    ('Завтрак', '55A15E', 'breakfast'),
+    ('Завтрак', '#', 'breakfast'),
+    ('Завтрак', '#5', 'breakfast'),
+    ('Завтрак', '#55', 'breakfast'),
+])
+def test_model_check_constraint(tag_name: str, tag_color: str, tag_slug: str, tag_factory: TagFactory) -> None:
+    """Test tag model CheckConstraint."""
+    with pytest.raises(IntegrityError):
+        tag_factory.create(
+            name=tag_name,
+            color=tag_color,
+            slug=tag_slug,
+        )
