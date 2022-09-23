c7c56898c4d4dc74bd1e6faa2decf05823d7380c
savilard <vitaliyantonoff@gmail.com>
2022-09-23 10:10:40 +0600

    :bug: Fix media display

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,10 +1,12 @@
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include
-from django.urls import path
+from django.urls import path, include
+
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
     path('api/', include('config.routers')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
