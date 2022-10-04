484c0b4f42b74d7cfa8760659d245c159cd2e36a
savilard <vitaliyantonoff@gmail.com>
2022-10-04 16:46:09 +0600

    :white_check_mark: Add test_get_recipes_success

---
+++ backend/tests/test_api/test_recipes/test_recipes_list.py
@@ -0,0 +1,26 @@
+from django.urls import reverse
+
+import pytest
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from tests.factories import RecipeFactory
+
+pytestmark = [pytest.mark.django_db]
+
+GET_RECIPE_LIST_URL = reverse('api:recipes-list')
+TOTAL_RECIPES_NUMBER = 3
+
+
+def test_get_recipes_success(recipe_factory: RecipeFactory, api_client: APIClient) -> None:
+    """Test getting recipes is successful.
+
+    Args:
+        recipe_factory: recipe factory
+        api_client: django rest framework api client
+    """
+    recipe_factory.create_batch(size=TOTAL_RECIPES_NUMBER)
+    response = api_client.get(GET_RECIPE_LIST_URL)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()['count'] == TOTAL_RECIPES_NUMBER
