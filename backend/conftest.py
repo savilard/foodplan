d2bf831ad2c3982339654dc56584ae6998bf4bdc
savilard <vitaliyantonoff@gmail.com>
2022-10-04 12:17:30 +0600

    :white_check_mark: Add api clients pyttest fixtures

--- backend/conftest.py
+++ backend/conftest.py
@@ -1,4 +1,7 @@
+import pytest
 from pytest_factoryboy import register
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APIClient
 
 from tests.factories.ingredient_factory import IngredientFactory
 from tests.factories.recipe_factory import RecipeFactory
@@ -9,3 +12,16 @@ register(IngredientFactory)
 register(RecipeFactory)
 register(UserFactory)
 register(TagFactory)
+
+
+@pytest.fixture
+def api_client():
+    return APIClient()
+
+
+@pytest.fixture
+def api_user_client(api_client: APIClient, user_factory: UserFactory) -> APIClient:
+    author = user_factory.create()
+    token = Token.objects.create(user=author)
+    api_client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+    return api_client
