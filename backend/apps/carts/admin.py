15402f7aeb93f4a8ab2195cb9e3819035ecf0ad1
savilard <vitaliyantonoff@gmail.com>
2022-10-08 13:07:19 +0600

    :sparkles: Add cart model

--- backend/apps/carts/admin.py
+++ backend/apps/carts/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.carts.models import Cart
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    show_full_result_count = False
+    fieldsets = (
+        (
+            None,
+            {
+                'fields': (
+                    'owner',
+                    'recipes',
+                ),
+            },
+        ),
+        (
+            'Техническая информация',
+            {
+                'classes': (
+                    'collapse',
+                ),
+                'fields': (
+                    'id',
+                ),
+            },
+        ),
+    )
+
+    readonly_fields = (
+        'id',
+    )
+    list_display = (
+        'owner',
+    )
+    raw_id_fields = (
+        'owner',
+    )
+    search_fields = (
+        'owner__username',
+        'owner__email',
+    )
--- backend/apps/carts/models.py
+++ backend/apps/carts/models.py
@@ -1,1 +1,26 @@
+from django.conf import settings
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from apps.core.models import BaseModel
+
+
+class Cart(BaseModel):
+    owner = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        related_name='cart',
+        verbose_name=_('Owner'),
+        on_delete=models.CASCADE,
+    )
+    recipes = models.ManyToManyField(
+        'recipes.Recipe',
+        verbose_name=_('Recipes'),
+        related_name='carts',
+    )
+
+    class Meta:
+        verbose_name = _('Shopping list')
+        verbose_name_plural = _('Shopping lists')
+
+    def __str__(self) -> str:
+        return '_("Shopping list"): {owner_full_name}'.format(owner_full_name=self.owner.full_name)
