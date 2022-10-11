3244f04bbdac002987a0a32fdbdc42fd2e92bfef
savilard <vitaliyantonoff@gmail.com>
2022-10-11 14:05:32 +0600

    :rotating_light: Make flake8 happy

--- backend/apps/core/management/commands/wait_for_db.py
+++ backend/apps/core/management/commands/wait_for_db.py
@@ -16,8 +16,9 @@ class Command(BaseCommand):
         while db_up is False:
             try:
                 self.check(databases=['default'])
-                db_up = True
             except (Psycopg2OpError, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
+                continue
+            db_up = True
         self.stdout.write(self.style.SUCCESS('Database available!'))
